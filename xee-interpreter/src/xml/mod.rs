a7104e8d820ba7d0ecd3b61647b18f927ac8fcdf
Martijn Faassen <faassen@startifact.com>
2025-04-09 12:50:48 +0200

    Just in time annotations seem to work on the internal test suite.

--- xee-interpreter/src/xml/mod.rs
+++ xee-interpreter/src/xml/mod.rs
@@ -1,11 +1,12 @@
 /// XML integration.
 mod annotation;
+mod annotation2;
 mod base;
 mod document;
 mod kind_test;
 mod step;
 
-pub use annotation::Annotations;
+pub(crate) use annotation2::DocumentOrderAccess;
 pub(crate) use base::BaseUriResolver;
 pub use document::{Document, DocumentHandle, Documents, DocumentsError};
 pub(crate) use kind_test::kind_test;
