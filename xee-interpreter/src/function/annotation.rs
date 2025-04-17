beded3d3e120977437de22b1ea74a9bfc66c8486
Martijn Faassen <faassen@startifact.com>
2025-04-17 16:19:20 +0200

    Extend the last boundary if possible.

--- xee-interpreter/src/function/annotation.rs
+++ xee-interpreter/src/function/annotation.rs
@@ -2,12 +2,12 @@
 // not to be confused with the annotations to XML nodes
 
 #[derive(Debug, Clone)]
-pub struct Annotations<T> {
+pub struct Annotations<T: PartialEq> {
     boundaries: Vec<usize>,
     data: Vec<T>,
 }
 
-impl<T> Annotations<T> {
+impl<T: PartialEq> Annotations<T> {
     pub fn new() -> Self {
         Annotations {
             boundaries: vec![0],
@@ -16,6 +16,17 @@ impl<T> Annotations<T> {
     }
 
     pub fn emit(&mut self, size: usize, data: T) {
+        // if what we're pushing is the same as the last time
+        if self.data.last() == Some(&data) {
+            // we just extend the last boundary
+            let last = self
+                .boundaries
+                .last_mut()
+                .expect("There should always be a last boundary");
+            *last += size;
+            return;
+        }
+
         let last = self
             .boundaries
             .last()
@@ -34,6 +45,12 @@ impl<T> Annotations<T> {
             // always safe as we cannot find 0 as the insertion point
             .unwrap_or_else(|x| x - 1);
         self.data.get(pos)
+    }
+}
+
+impl Default for Annotations<()> {
+    fn default() -> Self {
+        Annotations::new()
     }
 }
 
@@ -69,6 +86,27 @@ mod tests {
         assert_eq!(annotations.get(3), Some(&DummyData(1)));
         assert_eq!(annotations.get(4), Some(&DummyData(2)));
         assert_eq!(annotations.get(5), Some(&DummyData(2)));
+        assert_eq!(annotations.get(6), Some(&DummyData(3)));
+        assert_eq!(annotations.get(7), Some(&DummyData(3)));
+        assert_eq!(annotations.get(8), Some(&DummyData(3)));
+        assert_eq!(annotations.get(9), None);
+    }
+
+    #[test]
+    fn test_extend() {
+        let mut annotations: Annotations<DummyData> = Annotations::new();
+        annotations.emit(4, DummyData(1));
+        annotations.emit(2, DummyData(1));
+        annotations.emit(3, DummyData(3));
+
+        // we peek behind the curtain and see we only have 1 copy of DummyData(1)
+        assert_eq!(annotations.data.len(), 2);
+        assert_eq!(annotations.get(0), Some(&DummyData(1)));
+        assert_eq!(annotations.get(1), Some(&DummyData(1)));
+        assert_eq!(annotations.get(2), Some(&DummyData(1)));
+        assert_eq!(annotations.get(3), Some(&DummyData(1)));
+        assert_eq!(annotations.get(4), Some(&DummyData(1)));
+        assert_eq!(annotations.get(5), Some(&DummyData(1)));
         assert_eq!(annotations.get(6), Some(&DummyData(3)));
         assert_eq!(annotations.get(7), Some(&DummyData(3)));
         assert_eq!(annotations.get(8), Some(&DummyData(3)));
